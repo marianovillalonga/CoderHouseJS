0da2fd08263dc995f7d0bd6294a6c41452f25990
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2021-12-18 13:09:30 -0300

    Add files via upload

--- js/helper.js
+++ js/helper.js
@@ -139,3 +139,42 @@ $("#botonSelec").on("change" , function(e){
 
     console.log( e.target.value);
 });
+
+$(function(){
+    var getTxt = $('.content p').text();
+    var realTxt = [getTxt]
+    var textCol = [getTxt]
+    var wordsCount = textCol[0].split(' ').length;
+    var words = textCol[0].split(' ');
+    var wordsLimit = 40;
+    var limitCol;
+    
+    $('.readmore h3').click(function(){
+    if ( $(this).text() === "Ver mas") {
+        $('.content p').text(realTxt.join(' '));
+        $('.content').addClass('scroll');
+        $(this).text('Ver menos');
+        $('.item').addClass('showfull');
+      } else {
+        $('.content p').text(limitCol);
+         $(this).text('Ver mas');
+        $('.item').removeClass('showfull');
+      }
+  });
+    
+    if (wordsLimit <= wordsCount) {
+      words.splice(wordsLimit, wordsCount - wordsLimit);
+      words = words.join(' ');
+      $('.content p').text(words + '...');
+      limitCol = $('.content p').text();
+    }
+    
+    $('.like').click(function(){
+      $(this).toggleClass('clicked');
+    });
+    
+    
+    
+    
+    
+  });
